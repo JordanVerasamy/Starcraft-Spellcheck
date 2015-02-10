19f4a8f856996d030951366b59d11ce540bc7815
Jordan Verasamy <j.verasamy@gmail.com>
2015-02-10 01:53:54 -0500

    editing comment body

--- redditbot.py
+++ redditbot.py
@@ -1,7 +1,7 @@
 import praw
 import time
 
-r = praw.Reddit('experimenting with reddit python api')
+r = praw.Reddit('Experimenting with reddit python api')
 
 r.login('starcraftspellcheck', 'swarmhost')
 
@@ -25,8 +25,11 @@ while True:
 					has_typo = True
 
 			if has_typo and comment.id not in already_done:
-				comment.reply('A typo was detected. Did you accidentally type "swarmhost" when you meant to type "cancer"?')
+				comment.reply('A typo was detected. Did you accidentally type "swarmhost" or ' 
+							+ '"swarm host" when you meant to type "cancer bullshit unit"? This sort of '
+							+ 'typo is common, so I am a bot meant to help people remember not to make '
+							+ 'that typo. Hope this helped! :)')
 				already_done.add(comment.id)
 
-	print 'Iteration complete'
-	time.sleep(1000)
+	print 'Iteration complete.'
+	time.sleep(2000)
